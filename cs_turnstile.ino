a6ec2c95cf74ba27ee8b6aad8ebd7baed879dca9
hansfab65 <hansfab65@yahoo.it>
2022-12-22 17:11:29 +0100

    modified:   cs_turnstile.ino 	new file:   src/include/clConfig.h 	delete: src/include/config.h 	new file: src/include/clCentral.h

--- cs_turnstile.ino
+++ cs_turnstile.ino
@@ -1,5 +1,19 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <ArduinoBLE.h>
+
+// Service
+BLEService CS_AccessSystemService("AF000001-AFAF-A000-F000-A0F011110001");
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);  
+
+  // Inizializzazione
+  BLE.begin();
+  BLE.setLocalName("CS_BLE_Turnstile1");  // Ciascun Tornello ha un pogressivo diverso
 
 }
 
